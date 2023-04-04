df07dd9de54d6f34b14514d5a95f43475d95e68e
namtruong <lactroimtp1v@gmail.com>
2023-04-04 15:19:18 +0700

    update

--- src/pages/Upload/index.js
+++ src/pages/Upload/index.js
@@ -5,111 +5,105 @@ import { Modal, Select } from 'antd';
 import { useAuth } from "~/firebase";
 import images from "~/assets/images";
 import { getFirestore, collection, addDoc } from 'firebase/firestore';
-import {openNotificationSuccess ,openNotificationErorr} from '~/components/Notification'
+import { openNotificationSuccess, openNotificationErorr } from '~/components/Notification'
 import { uploadVideo } from "~/firebase";
 
 const firestore = getFirestore();
-const postVideos = collection(firestore,"postVideo")
+const postVideos = collection(firestore, "postVideo")
 
 // upload Video 
 
- 
+
 const cx = classNames.bind(styles)
 
-function Upload(){
+function Upload() {
     const currentUser = useAuth();
-    
+
     const [loading, setLoading] = useState(false);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [video, setVideo] = useState('');
     const [tagsVideo, setTagsVideo] = useState({});
     const [dataVideo, setDataVideo] = useState({
-        id:'',
-        title:"",
-        account :{
-            id:'',
-            name_acount:'',
-            nick_name:'',
-            image:''
+        id: '',
+        title: "",
+        account: {
+            id: '',
+            name_acount: '',
+            nick_name: '',
+            image: ''
         },
-        music:'',
-        hastag:{},
-        src_video:'',
-        tick:false
+        music: '',
+        hastag: {},
+        src_video: '',
+        tick: false
     });
-    const [description,setDescription] = useState('');
+    const [description, setDescription] = useState('');
 
-    useEffect(()=>{
-        if(currentUser){
-        //    console.log(currentUser.uid)
-        setDataVideo({
-            id:Math.random(),
-            title:description,
-            account :{
-                id: currentUser.uid,
-                name_acount:currentUser.displayName,
-                nick_name: currentUser.email,
-                image:currentUser.photoURL != null ? currentUser.photoURL : images.noImage
-            },
-            music:'',
-            hastag:tagsVideo,
-            src_video:video,
-            tick:true,
-        })
-           
+    useEffect(() => {
+        if (currentUser) {
+            //    console.log(currentUser.uid)
+            setDataVideo({
+                id: Math.random(),
+                title: description,
+                account: {
+                    id: currentUser.uid,
+                    name_acount: currentUser.displayName,
+                    nick_name: currentUser.email,
+                    image: currentUser.photoURL != null ? currentUser.photoURL : images.noImage
+                },
+                music: '',
+                hastag: tagsVideo,
+                src_video: video,
+                tick: true,
+            })
+
         }
-    },[currentUser])
+    }, [currentUser])
 
 
     const showModal = () => {
         setIsModalOpen(true);
     };
     const handleOk = async () => {
-        
-       
-           
-          try{
+
+        try {
             setLoading(true)
             await addDoc(postVideos, {
-                id:Math.random(),
-                title:description,
-                account :{
+                id: Math.random(),
+                title: description,
+                account: {
                     id: currentUser.uid,
-                    name_acount:currentUser.displayName,
+                    name_acount: currentUser.displayName,
                     nick_name: currentUser.email,
-                    image:currentUser.photoURL != null ? currentUser.photoURL : images.noImage
+                    image: currentUser.photoURL != null ? currentUser.photoURL : images.noImage
                 },
-                music:'',
-                hastag:tagsVideo,
-                src_video:video,
-                tick:true,
+                music: '',
+                hastag: tagsVideo,
+                src_video: video,
+                tick: true,
             });
             console.log('thành công');
             setLoading(false);
             setIsModalOpen(false);
-            openNotificationSuccess("topRight",'Thành công !','Video của bạn đã được tải lên !')
+            openNotificationSuccess("topRight", 'Thành công !', 'Video của bạn đã được tải lên !')
 
-          }catch(error){
-            openNotificationErorr("topRight",'Thất bại !','Dường như có lỗi sảy ra')
-            console.log(error,'có lõi')
-          }
-             
-            
-          
-      
-        
-        
+        } catch (error) {
+            openNotificationErorr("topRight", 'Thất bại !', 'Dường như có lỗi sảy ra')
+            console.log(error, 'có lõi')
+        }
+
     };
+
     const handleCancel = () => {
         setIsModalOpen(false);
     };
 
     const options = [];
     for (let i = 10; i < 36; i++) {
-    options.push({
-        value: i.toString(36) + i,
-        label: i.toString(36) + i,
-    });
+        options.push({
+            value: i.toString(36) + i,
+            label: i.toString(36) + i,
+        });
     }
 
     const handleChange = (value) => {
@@ -117,33 +111,33 @@ function Upload(){
         console.log(value)
     };
 
-    const handleChangeVideo = async (e) =>{
+    const handleChangeVideo = async (e) => {
         const file = e.target.files[0];
 
         file.preview = URL.createObjectURL(file);
-        const urlFile = await uploadVideo(file,currentUser,setLoading)
+        const urlFile = await uploadVideo(file, currentUser, setLoading)
         setVideo(urlFile);
     }
 
 
     return (
-        <div style={{marginTop:100}}>
-                <div className={cx("jsx-2674289056 upload-container")}>
-                    <div className={cx("jsx-2674289056 uploader")}>
-                        <div className={cx("jsx-4258277349 upload")}>
-                            <input type="file" accept="video/*" className={cx("jsx-4258277349")} style={{display: 'none'}} />
-                            <div   className={cx("jsx-4258277349", "upload-card", "before-upload-stage")}>
-                                <img src="//lf16-tiktok-common.ibytedtos.com/obj/tiktok-web-common-sg/ies/creator_center/svgs/cloud-icon1.ecf0bf2b.svg" className={cx("jsx-4258277349 cloud-icon")} />
-                                <div className={cx("jsx-4258277349", "text-main")}><span className={cx("css-1q42136")}>Chọn video để tải lên</span></div>
-                                <div className={cx("jsx-4258277349 ")}><span className={cx("css-1wbo2p7")}>Hoặc kéo và thả tập tin</span></div>
-                                <div className={cx("jsx-4258277349", "text-sub")}><span className={cx("css-1wbo2p7")}>Có thể tách video dài thành nhiều phần để tăng khả năng hiển thị</span></div>
-                                <div className={cx("jsx-4258277349", "text-video-info")}>
+        <div style={{ marginTop: 100 }}>
+            <div className={cx("jsx-2674289056 upload-container")}>
+                <div className={cx("jsx-2674289056 uploader")}>
+                    <div className={cx("jsx-4258277349 upload")}>
+                        <input type="file" accept="video/*" className={cx("jsx-4258277349")} style={{ display: 'none' }} />
+                        <div className={cx("jsx-4258277349", "upload-card", "before-upload-stage")}>
+                            <img src="//lf16-tiktok-common.ibytedtos.com/obj/tiktok-web-common-sg/ies/creator_center/svgs/cloud-icon1.ecf0bf2b.svg" className={cx("jsx-4258277349 cloud-icon")} />
+                            <div className={cx("jsx-4258277349", "text-main")}><span className={cx("css-1q42136")}>Chọn video để tải lên</span></div>
+                            <div className={cx("jsx-4258277349 ")}><span className={cx("css-1wbo2p7")}>Hoặc kéo và thả tập tin</span></div>
+                            <div className={cx("jsx-4258277349", "text-sub")}><span className={cx("css-1wbo2p7")}>Có thể tách video dài thành nhiều phần để tăng khả năng hiển thị</span></div>
+                            <div className={cx("jsx-4258277349", "text-video-info")}>
                                 <div className={cx("jsx-4258277349")}><span className={cx("css-tad11f")}>MP4 hoặc WebM</span></div>
                                 <div className={cx("jsx-4258277349")}><span className={cx("css-tad11f")}>Độ phân giải 720x1280 trở lên</span></div>
                                 <div className={cx("jsx-4258277349")}><span className={cx("css-tad11f")}>Tối đa 30 phút</span></div>
                                 <div className={cx("jsx-4258277349")}><span className={cx("css-tad11f")}>Nhỏ hơn 2 GB</span></div>
-                                </div>
-                                <div className={cx("jsx-4258277349 file-select-button wide-file-select-button")}>
+                            </div>
+                            <div className={cx("jsx-4258277349 file-select-button wide-file-select-button")}>
                                 <button onClick={showModal} className={cx("css-byn4hh")}>
                                     <div className={cx("css-1db5cpb")}>
                                         <div className={cx("css-1z070dx")}>Chọn tập tin</div>
@@ -152,36 +146,36 @@ function Upload(){
 
                                 <Modal title="Upload Video" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
                                     <div className={cx('wrapper_modal')}>
-                                    <form >
-                                            {loading && (<div className={cx('lds-ring')}><div></div><div></div><div></div><div></div></div>)}                                       
+                                        <form >
+                                            {loading && (<div className={cx('lds-ring')}><div></div><div></div><div></div><div></div></div>)}
                                             <div className={cx('change-flies-box')}>
                                                 <video className={cx('video-preview')} playsInline muted controls src={video} > </video>
-                                                <input  onChange={handleChangeVideo} type='file'  />
-                                                <input className={cx('description_video')} value={description} onChange={(e)=>{setDescription(e.target.value)}} type='text' placeholder="Mô tả Video" />
+                                                <input onChange={handleChangeVideo} type='file' />
+                                                <input className={cx('description_video')} value={description} onChange={(e) => { setDescription(e.target.value) }} type='text' placeholder="Mô tả Video" />
                                                 <Select
                                                     mode="tags"
                                                     style={{
-                                                    width: '100%',
+                                                        width: '100%',
                                                     }}
                                                     placeholder="Tags Video"
                                                     onChange={handleChange}
                                                     options={options}
                                                 />
-                                            </div>                                          
-                                        </form> 
-                                    
+                                            </div>
+                                        </form>
+
 
                                     </div>
-                                    
+
                                 </Modal>
-                                </div>
                             </div>
                         </div>
                     </div>
-                    </div>
-                            
+                </div>
+            </div>
+
         </div>
-       
+
     )
 }
 
