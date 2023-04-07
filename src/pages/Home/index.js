d5fdde739b1f3fe9f0cdd81ab63bf3556a16eec7
namtruong <lactroimtp1v@gmail.com>
2023-04-07 16:09:50 +0700

    update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,46 +1,46 @@
 import { useState, useEffect } from 'react';
 import Video from "~/components/Video"
 
-import { collection,getFirestore, getDocs } from "firebase/firestore";
+import { collection, getFirestore, getDocs } from "firebase/firestore";
 
 
 
-function Home(){
+function Home() {
 
     const [videos, setVideos] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         const firestore = getFirestore();
-            const fechVideo = async () =>{
-                const q = collection(firestore, "postVideo");
-                const data = await getDocs(q);
-                setVideos(data.docs.map((doc)=> ({...doc.data()})))
-                
-          
-            }
-            fechVideo();
-    },[])
- 
+        const fechVideo = async () => {
+            const q = collection(firestore, "postVideo");
+            const data = await getDocs(q);
+            setVideos(data.docs.map((doc) => ({ ...doc.data() })))
+
+
+        }
+        fechVideo();
+    }, [])
+
     return (
         <div className="wrapper">
 
             {
-                videos.map((item)=>(
-                    <Video 
-                    key={item.id} 
-                    id={item.id} 
-                    account={item.account}
-                    title ={item.title}  
-                    music={item.music}
-                    hastag={item.hastag} 
-                    src_video={item.src_video}
-                    tick={item.tick}
+                videos.map((item) => (
+                    <Video
+                        key={item.id}
+                        id={item.id}
+                        account={item.account}
+                        title={item.title}
+                        music={item.music}
+                        hastag={item.hastag}
+                        src_video={item.src_video}
+                        tick={item.tick}
                     />
                 ))
             }
 
-           
-            
+
+
         </div>
     )
 }
